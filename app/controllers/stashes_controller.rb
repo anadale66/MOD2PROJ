3e5097bc7e3652e5867f7e4573d1bdf8108f78c6
Flatiron School <flatironschool@Flatirons-MacBook-Air-96.local>
2018-12-30 21:13:38 -0500

    Adds Stashes CRUD

--- app/controllers/stashes_controller.rb
+++ app/controllers/stashes_controller.rb
@@ -1,2 +1,50 @@
 class StashesController < ApplicationController
+  def index
+    @stashes = Stash.all 
+  end
+
+  def show
+    @stash = Stash.find(params[:id])
+  end
+
+  def new
+    @stash = Stash.new 
+  end
+
+  def create
+     @stash = Stash.create(stash_params) 
+
+    flash[:notice] = "stash created!"    
+    redirect_to @stash
+  end
+
+  def edit
+     @stash = Stash.find(params[:id])
+  end
+
+  def update
+    @stash = Stash.find(params[:id])
+    @stash.update(stash_params)
+
+    flash[:notice] = "stash updated!"
+    redirect_to @stash 
+  end
+
+  def destroy
+    @stash = Stash.find(params[:id])
+    @stash.destroy 
+    
+    flash[:notice] = "#{@stash.name} Deleted!"
+    redirect_to stashes_path 
+  end
+
+private 
+
+  def stash_params
+	  params.require(:stash).permit(:name, :squirrel_id, :snack_id) 
+  end
+
+
+
+
 end
--- app/models/squirrel.rb
+++ app/models/squirrel.rb
@@ -2,4 +2,6 @@ class Squirrel < ApplicationRecord
     belongs_to :avatar 
     has_many :stashes
     has_many :snacks, through: :stashes
+    validates :name, uniqueness:true
 end
+
