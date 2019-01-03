ee9d53038ac37c6650c92e991d028e4aee4b244c
Flatiron School <flatironschool@FlatironsAir96.home>
2019-01-03 00:33:43 -0500

    Adds some buttons and squirrel space link button

--- app/models/squirrel.rb
+++ app/models/squirrel.rb
@@ -2,6 +2,6 @@ class Squirrel < ApplicationRecord
     belongs_to :avatar 
     has_many :stashes
     has_many :snacks, through: :stashes
-    validates :name, uniqueness:true
+    # validates :name, uniqueness:true
 end
 
